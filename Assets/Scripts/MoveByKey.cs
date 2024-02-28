b534aaebba0bf7ea506533db541da718ec37e200
MQHA\Administrator <hunganh7793@gmail.com>
2024-02-28 21:13:29 +0700

    no message

---
+++ Assets/Scripts/MoveByKey.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByKey : MonoBehaviour
+{
+    public CharacterController characterController;
+    [SerializeField] private float movingSpeed;
+
+    private void OnValidate()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        Vector3 direction = (transform.right * hInput + transform.forward * vInput).normalized;
+        characterController.SimpleMove(direction * movingSpeed);
+
+    }
+}
