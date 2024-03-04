f38e0e44f52ea632bc66f9587cfe55acfdf9a39b
MQHA\Administrator <hunganh7793@gmail.com>
2024-03-04 20:04:27 +0700

    no message

---
+++ Assets/Scripts/GrenadeLauncher.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeLauncher : MonoBehaviour
+{
+    private const int LeftMouseButton = 0;
+    public GameObject bulletPrefab;
+    public Transform firingPos;
+    public float bulletSpeed = 5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(LeftMouseButton))
+        {
+            AddProjectile();
+        }
+    }
+
+    public void AddProjectile()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firingPos.position, firingPos.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = firingPos.forward * bulletSpeed;
+    }
+}
